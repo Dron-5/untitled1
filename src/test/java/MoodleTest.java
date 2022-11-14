4cea350fd0654e0be7f18400709e848f0c08d42e
xa4ipur4ik <kirill.nowoselow@mail.ru>
2022-11-14 13:16:38 +0300

    а

--- src/test/java/MoodleTest.java
+++ src/test/java/MoodleTest.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,7 +23,7 @@ public class MoodleTest {
     private SelenideElement ReqID = $(By.xpath("//input[@class='required input-small amount']"));
     private SelenideElement ForID = $(By.xpath("//button[@id='forward']"));
     private SelenideElement PID = $(By.xpath("//input[@type='checkbox']"));
-    private SelenideElement ButtID = $(By.xpath("//button[@id='confirm']"));
+
 
 
 
@@ -43,22 +44,36 @@ public class MoodleTest {
     @Test
     public void test()
     {
-        System.out.println();
+        step1();
+        step2();
+        step3();
+        step4();
+
+    }
+
+    @Step("Авторизация")
+    private void step1(){
         loginInput.should(Condition.visible).val("demo");
         passwordInput.should(Condition.visible).val("demo");
         loginBtn.should(Condition.visible).click();
         BtnId.should(Condition.visible).click();
+    }
+    @Step("Перевод на карту")
+    private void step2(){
         PymId.should(Condition.visible).click();
         LinID.should(Condition.visible).click();
         DatID.should(Condition.visible).click();
+    }
+    @Step("Ввод номера карты и суммы для перевода")
+    private void step3(){
         TXID.should(Condition.visible).val("1156 1565 1561 5616");
         ReqID.should(Condition.visible).val("100");
+    }
+    @Step("Переходим далее и соглашаемся с Правилами")
+    private void step4(){
         ForID.should(Condition.visible).click();
         PID.should(Condition.visible).click();
-        ButtID.should(Condition.visible).click();
-        sleep(1000000000);
     }
-
     @AfterEach
     public void after()
     {
